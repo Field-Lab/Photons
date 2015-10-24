0d4dd2a052b08df15dbccc1f11ebe6d3cca19e51
vision <vision@fire-mac.stanford.edu>
2015-10-24 14:44:58 -0700

    moving grating textures in make_stimulus

--- Utils/save_parameters.m
+++ Utils/save_parameters.m
@@ -5,9 +5,9 @@ if ~isdir(dir_save)
     mkdir(dir_save);
 end
 
-tmp = dir([dir_save, 'stimulus_*.mat']);
+tmp = dir([dir_save, '/stimulus_*.mat']);
 stim_number = int2str(length(tmp)+1);
 
 parameters = stimulus.parameters;
 
-save([dir_save, 'stimulus_', stim_number], 'parameters');
+save([dir_save, '/stimulus_', stim_number], 'parameters');
--- Utils/save_time_stamps.m
+++ Utils/save_time_stamps.m
@@ -5,13 +5,13 @@ if ~isdir(dir_save)
     mkdir(dir_save);
 end
 
-tmp = dir([dir_save, 'stimulus_*.mat']);
+tmp = dir([dir_save, '/stimulus_*.mat']);
 stim_number = int2str(length(tmp)+1);
 
-tmp1 = dir([fullfile(path2save, data_name,'parameters', filesep), 'stimulus_*.mat']);
+% tmp1 = dir([fullfile(path2save, data_name,'parameters', filesep), 'stimulus_*.mat']);
+% 
+% if length(tmp) ~= length(tmp1)
+%     errordlg({'Parameters and time stamps file numbers do NOT match!'},'!! Warning !!')    
+% end
 
-if length(tmp) ~= length(tmp1)
-    errordlg({'Parameters and time stamps file numbers do NOT match!'},'!! Warning !!')    
-end
-
-save([dir_save, 'stimulus_', stim_number], 'time_stamps');
+save([dir_save, '/stimulus_', stim_number], 'time_stamps');
