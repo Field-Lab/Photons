f7932d390df3aea6c201a3e8995efe1d78cda6e1
Alexandra <alexth.nn@gmail.com>
2015-12-15 13:39:45 -0800

    fixed start movie frame

--- Utils/Make_Frame_RawMovie.m
+++ Utils/Make_Frame_RawMovie.m
@@ -6,5 +6,5 @@ if stimulus.reverse == 1 % reverse mode
 else % forward mode
     current_frame = stimulus.start_frame + movie_frame;
 end
-
+current_frame = current_frame - 1;
 [~, img_frame] = Read_RawMov_Frame(stimulus.movie_filename, stimulus.orig_width, stimulus.orig_height, stimulus.header_size, current_frame, stimulus.flip);
