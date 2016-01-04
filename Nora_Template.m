3c42b4748a7e86c3dbd1113c627182af8cf25527
vision <vision@fire.local>
2016-01-04 15:02:24 -0800

    edit Nora template

--- Nora_Template.m
+++ Nora_Template.m
@@ -13,15 +13,15 @@ clear parameters stimulus time_stamps
 dataname = 'data000';
 
 % For repeats
-%%{
+%{
 repeats = 3;
 parameters.frames = 120;
 %}
 
 % For straight through
-%{
+%%{
 repeats = 1; % for straight through
-parameters.frames = 120*10;
+parameters.frames = 120*1000;
 %}
 
 % For masking
@@ -40,22 +40,22 @@ parameters.independent = 1;
 parameters.binary = 1;
 parameters.probability = 1;
 parameters.delay_frames = 0;
-parameters.frames = 120;
+%parameters.frames = 120;
 
 %%{
 parameters.x_start = 1;  parameters.x_end = 640;
 parameters.y_start = 81;   parameters.y_end = 400;
-parameters.stixel_width = 8;   parameters.stixel_height = 8;
-parameters.field_width = 80;  parameters.field_height = 40;
+parameters.stixel_width = 1;   parameters.stixel_height = 1;
+parameters.field_width = 640;  parameters.field_height = 320;
 %}
 
 stimulus = make_stimulus(parameters, def_params); 
 for i = 1:repeats
-    time_stamps{i} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_key',1, 'erase', 1);
+    time_stamps{i} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_trigger',0,'wait_key', 1, 'erase', 1);
 end
 % Save stuff
-save([path2save '/' dataname '_time_stamps.mat'], 'time_stamps');
-save_parameters(stimulus, path2save, dataname);
+%save([path2save '/' dataname '_time_stamps.mat'], 'time_stamps');
+%save_parameters(stimulus, path2save, dataname);
 
 %% Raw Movie
 
@@ -65,14 +65,14 @@ clear parameters stimulus time_stamps;
 dataname = 'data000';
 
 % For repeats
-%%{
+%{
 repeats = 3;
 parameters.frames = 120;
 interleaved = 0;
 %}
 
 % For straight through
-%{
+%%{
 repeats = 1; % for straight through
 parameters.frames = 120*10;
 interleaved = 0;
@@ -99,7 +99,7 @@ parameters.mask = mask;
 % Movie Name
 % parameters.movie_name = '/Volumes/Data/Stimuli/movies/eye-movement/current_movies/NSbrownian/NSbrownian_3000_movies/NSbrownian_3000_A_025.rawMovie';
 % parameters.movie_name = '/Volumes/Lab/Users/Nora/new_stim_nora/mask_NSEM/testmask_3_stix2/comp_LES/movie_3_comp_LES.rawMovie';
-parameters.movie_name = '/Users/vision/Desktop/NSbrownian_3000_A_025.rawMovie';
+parameters.movie_name = '/Users/vision/Desktop/Stimuli/NSbrownian_3000_A_025.rawMovie';
 % Don't need to change
 parameters.class = 'RM';
 parameters.back_rgb = [1 1 1]*0.25;
@@ -115,13 +115,13 @@ parameters.reverse = 0;   % 1 = backward (reverse), 0 = forward
 stimulus = make_stimulus(parameters, def_params); 
 count = 1;
 for i = 1:repeats
-    time_stamps{count} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_trigger',1, 'erase', 1);
+    time_stamps{count} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_trigger',0, 'erase', 1);
     count = count + 1;
     if interleaved
         parameters.start_frame = start_frame_fitting;
         parameters.frames = frames_fitting;
         stimulus = make_stimulus(parameters, def_params);
-        time_stamps{count} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_key',1, 'erase', 1);
+        time_stamps{count} = display_stimulus(stimulus, 'trigger_interval', 100, 'wait_key',0, 'erase', 1);
         count = count + 1;
         parameters.start_frame = start_frame_testing;
         parameters.frames = frames_testing;
@@ -131,5 +131,5 @@ for i = 1:repeats
 end
 
 % Save stuff
-save([path2save '/' dataname '_time_stamps.mat'], 'time_stamps');
-save_parameters(stimulus, path2save, dataname);
+% save([path2save '/' dataname '_time_stamps.mat'], 'time_stamps');
+% save_parameters(stimulus, path2save, dataname);
